35ade7972bece226519ad9b25fed74455e3cb803
Polina Fedorova <fedorovapolina94@gmail.com>
2017-04-01 19:15:13 +0300

    style for buttons 1-4

--- js/script.js
+++ js/script.js
@@ -51,17 +51,20 @@ var content1 = $(".content1");
 var content2 = $(".content2");
 var content3 = $(".content3");
 var content4 = $(".content4");
+var final = $('.final');
 
 var template = function () {
-    /*content1.hide();*/
+    button1.css('color','black');
+    button2.css('color','#bbbbbb');
+    button3.css('color','#bbbbbb');
+    button4.css('color','#bbbbbb');
+    content1.show();
     content2.hide();
     content3.hide();
     content4.hide();
+    final.hide();
     /*console.log(window.innerHeight);*/
-    /*
-     innerwrap.prepend("<div class=\"sections header\"><button class=\"header__button header__button1\">1</button>    <button class=\"header__button header__button2\">2</button>    <button class=\"header__button header__button3\">3</button>    <button class=\"header__button header__button4\">4</button>    <p class=\"header__text\">1. Введите имя и e-mail</p></div>");
-     innerwrap.append("<form action=\"\"><div class=\"sections content\">    <input type=\"text\" class=\"content__input content__name\" placeholder=\"Имя\"> <br>    <input type=\"text\" class=\"content__input content__email\" placeholder=\"E-mail\">    </div>    <div class=\"sections switch\">    <button class=\"switch__button switch__prev\">Предыдущий</button>    <button class=\"switch__button switch__next\">Следующий</button>    </div>    </form>");
-     */
+
 };
 
 window.onload = template;
@@ -71,19 +74,56 @@ if (window.innerHeight < 440) {
     $('.wrapper').css('height', 'auto');
 }
 
-
-
-
-/*------STEP1--------*/
-
-
+/*windows changing*/
 
 var switchCounter = 1;
-var headerText=$("p.header__text");
+var headerText = $("p.header__text");
 /*content1.show();*/
 
+var switchPrev=$('.switch__prev');
+var switchNext=$(".switch__next");
 
-$('.switch__prev').on('click', function () {
+
+var case1 = function () {
+    headerText.text("1. Введите имя и e-mail");
+    switchNext.css('color', '#ff9800').css('background-color', 'white').text('Следующий');
+    button1.css('color','black');
+    content1.show();
+    content2.hide();
+    content3.hide();
+    content4.hide();
+};
+var case2 = function () {
+    headerText.text("2. Выберите страну и город");
+    switchNext.css('color', '#ff9800').css('background-color', 'white').text('Следующий');
+    button2.css('color','black');
+    content1.hide();
+    content2.show();
+    content3.hide();
+    content4.hide();
+};
+var case3 = function () {
+    headerText.text("3. Отметьте социальные сети");
+    switchNext.css('color', '#ff9800').css('background-color', 'white').text('Следующий');
+    button3.css('color','black');
+    content1.hide();
+    content2.hide();
+    content3.show();
+    content4.hide();
+};
+var case4 = function () {
+    headerText.text("4. Выберите любимого котика");
+    switchNext.css('color', 'white').css('background-color', '#ff9800').text('Завершить');
+    button4.css('color','black');
+    content1.hide();
+    content2.hide();
+    content3.hide();
+    content4.show();
+};
+
+
+
+switchPrev.on('click', function () {
     /*if (switchCounter == 1) {
      return false;
      }*/
@@ -93,75 +133,116 @@ $('.switch__prev').on('click', function () {
             break;
         case 2:
             switchCounter--;
-            console.log(switchCounter);
-            headerText.text("1. Введите имя и e-mail");
-            content1.show();
-            content2.hide();
-            content3.hide();
-            content4.hide();
+            case1();
+            button2.css('color','#ff9800');
             return false;
             break;
         case 3:
             switchCounter--;
-            console.log(switchCounter);
-            headerText.text("2. Выберите страну и город");
-            content1.hide();
-            content2.show();
-            content3.hide();
-            content4.hide();
+            case2();
+            button3.css('color','#ff9800');
             return false;
             break;
         case 4:
             switchCounter--;
-            console.log(switchCounter);
-            headerText.text("3. Отметьте социальные сети");
-            content1.hide();
-            content2.hide();
-            content3.show();
-            content4.hide();
+            case3();
+            button4.css('color','#ff9800');
             return false;
     }
 });
 
-$(".switch__next").on("click", function () {
+switchNext.on("click", function () {
     switch (switchCounter) {
         case 1:
             switchCounter++;
-            console.log(switchCounter);
-            user.name=$('.content__name').val();
-            user.email=$('.content__email').val();
-            headerText.text("2. Выберите страну и город");
-            content1.hide();
-            content2.show();
-            content3.hide();
-            content4.hide();
+            case2();
+            button1.css('color','#ff9800');
             return false;
             break;
         case 2:
             switchCounter++;
-            user.country=countries[contentCountry.val()];
-            user.city=cities[$('.content__city').val()].name;
-            headerText.text("3. Отметьте социальные сети");
-            content1.hide();
-            content2.hide();
-            content3.show();
-            content4.hide();
+            case3();
+            button2.css('color','#ff9800');
             return false;
             break;
-        case 3: switchCounter++;
-            /*user.social=;*/
-            headerText.text("4. Выберите любимого котика");
-            content1.hide();
-            content2.hide();
-            content3.hide();
-            content4.show();
+        case 3:
+            switchCounter++;
+            case4();
+            button3.css('color','#ff9800');
             return false;
             break;
         case 4:
-            /*user.image=;*/
+            $('.innerwrap').hide();
+            user.name = contentName.val();
+            user.email = contentEmail.val();
+            try {
+                user.country = countries[contentCountry.val()];
+                user.city = cities[$('.content__city').val()].name;
+            } catch (e) {
+            }
+            /*user.social=;*/
+            $('.final__name').text(user.name);
+            $('.final__email').text(user.email);
+            $('.final__location').text(user.country + ', ' + user.city);
+
+
+            final.show();
             return false;
     }
 });
+
+/*1-4 buttons*/
+
+var button1=$('.header__button1'),
+    button2=$('.header__button2'),
+    button3=$('.header__button3'),
+    button4=$('.header__button4');
+
+button1.on('click', function () {
+    button1.css('color','black');
+    switchCounter=1;
+    case1();
+    return false;
+});
+
+button2.on('click', function () {
+    button2.css('color','black');
+    switchCounter=2;
+    case2();
+
+    return false;
+});
+
+button3.on('click', function () {
+    button3.css('color','black');
+    switchCounter=3;
+    case3();
+
+    return false;
+});
+
+button4.on('click', function () {
+    button4.css('color','black');
+    switchCounter=4;
+    case4();
+
+    return false;
+});
+
+
+/*------STEP1--------*/
+
+
+
+var nameTemp = /.+/;
+var emailTemp = /.+@.+\..+/;
+var contentName = $('.content__name');
+var contentEmail = $('.content__email');
+/*if ( nameTemp.test(contentName.val()) && emailTemp.test(contentEmail.val()) == false ){
+    switchNext.off();
+
+}*/
+
 
 
 /*-----STEP2-------*/
@@ -172,8 +253,6 @@ var contentCity = $('.content__city');
 for (var i in countries) {
     contentCountry.append('<option class=\"content__option\" value=\"' + i + '\">' + countries[i] + '</option>');
 }
-/*var a=$('.content__country option:selected').val();
- console.log(a);*/
 
 contentCountry.on('change', function () {
     var selectedCountry = contentCountry.val();
@@ -188,16 +267,28 @@ contentCountry.on('change', function () {
 });
 
 
-
-
 /*------STEP3-----*/
 
+
+var socialCheck = $('.content__socialcheck');
+socialCheck.on('change', function (e) {
+    console.log();
+    $(e.target).parent().siblings().toggle();
+});
 
 
 /*------STEP4------*/
 
+var contentImages = $('.content__images');
 
 
 /*-------FINAL------*/
 
-/*innerwrap.html(user.name + '  ' + user.email);*/
+var finalReset=$('.final__reset');
+finalReset.on('click', function () {
+    switchCounter=1;
+    template();
+    $('.innerwrap').css('display', 'block');
+    switchNext.css('color', '#ff9800').css('background-color', 'white').text('Следующий');
+    headerText.text('1. Введите имя и e-mail');
+});
